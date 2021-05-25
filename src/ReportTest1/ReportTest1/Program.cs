a18e0539d33168feabac8950e1c27d97b7da6ba0
d.pujdak <denis.pujdak@hotmail.com>
2021-05-25 18:20:43 +0100

    sample

---
+++ src/ReportTest1/ReportTest1/Program.cs
@@ -0,0 +1,26 @@
+﻿using Stimulsoft.Report;
+using System;
+using System.IO;
+
+namespace ReportTest1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Printing the report..");
+            
+            var reportMrt = File.ReadAllText("TestReport.mrt");
+
+            using (var masterReport = new StiReport())
+            {
+                masterReport.LoadFromJson(reportMrt);
+                masterReport.Render();
+                masterReport.ExportDocument(StiExportFormat.Pdf, $"Report_{DateTime.Now.ToString("yyyyddMM_HHmmss")}.pdf");
+            }
+
+            Console.WriteLine("Report Generated.");
+            Console.ReadKey();
+        }
+    }
+}
